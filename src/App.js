68e96df474db993b1bff57c9cf3ecd610d6f78e4
dfunk0 <fdunkown1@gmail.com>
2023-03-30 17:02:42 -0300

    reafctor: unifying multiple markup elements into array structure

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function Board () {
+  const boardRows=[0,1,2].map(x => <BoardRow key={x}/>)
+
   return (
     <div className="board">
-      <BoardRow/>
-      <BoardRow/>
-      <BoardRow/>
+      {boardRows}
     </div>
   )
 };
 
 function BoardRow () {
+  const rowSquares = [0,1,2].map((x) => <Square key={x} checked="X" />)
+
   return (
       <div className="board-row">
-        <Square checked="X"/>
-        <Square checked="X"/>
-        <Square checked="X"/>
+        {rowSquares}
       </div>
   )
 }
